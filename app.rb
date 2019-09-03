ea29fc6f7e979c5f80fde3ac9fdb6ab9993c33bc
Chris Junker <elevatemedfit@gmail.com>
2019-09-03 17:00:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/name' do
+      "Started my server using Shotgun!"
+    end
 
-end
+  end
+
+  # medicines_controller.rb
+
+  # get '/medicines' do
+  #   @medicines = Medicine.all
+  #
+  #   erb :'medicines/index.html.erb'
+  # end
